34a856161f81792f118f392e78cbf9f80d486220
DJ Martin <umdjm@hotmail.com>
2013-10-04 22:13:27 -0400

    Initial Checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TapcoSalesforce::Application.config.secret_token = '6e0867c9dd810e0b1cee36836f392236c5c8ba5857f5abeb273e79e7c211a1fb0a3eb913a6b27010585a31b24c5d61a91dffa15c6f33ca7e12bc2e0717d3038c'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+TapcoSalesforce::Application.routes.draw do
+  get "static_pages/home"
+  root :to => 'static_pages#home'
+end
